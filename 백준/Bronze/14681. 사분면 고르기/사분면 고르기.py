202ea4154449aac722ad606c4dee699533d71d76
seulhui <75656859+leeseulhui@users.noreply.github.com>
2024-09-22 17:07:26 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+x = int(input())
+y = int(input())
+
+-1000 <= x <= 1000 and x != 0
+-1000 <= y <= 1000 and x != 0
+
+if x > 0 and y > 0:
+    print(1)
+elif x < 0 and y > 0:
+    print(2)  
+elif x < 0 and y < 0:
+    print(3)  
+else:
+    print(4)
