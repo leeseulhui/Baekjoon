322bd68872ca47f7d92dc95efcc2131903beadc4
seulhui <75656859+leeseulhui@users.noreply.github.com>
2024-10-07 18:08:50 +0900

    [Bronze V] Title: 사칙연산, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -3,16 +3,8 @@ input = sys.stdin.readline
 
 A, B = map(int, input().split())
 
-if 1 <= A <= 10000 and 1 <= B <= 10000:
-
-    sum = A + B
-    min = A - B
-    double = A * B
-    div = A // B
-    mod = A % B
-
-print(sum)
-print(min)
-print(double)
-print(div)
-print(mod)
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
