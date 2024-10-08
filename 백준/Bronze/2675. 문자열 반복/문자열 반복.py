1fabe66c2b6c8f2b804de37bfcaed54a05921cf2
seulhui <75656859+leeseulhui@users.noreply.github.com>
2024-10-08 12:04:08 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,17 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input())
+T = int(input().strip())
 
 for _ in range(T):
-    R, S = input().split()   #한 줄에서 나타내야 하니까 같이 써야함.
-    R = int(R)  
-    
-    P = ""  #결과 문자열 초기화함
+    R, S = input().strip().split()
+    R = int(R)
+
+    P = ""  
 
     for char in S:
         P = P + char * R
 
     print(P)
-    
+
+
+
     
