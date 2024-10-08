65f5e41111bb90dfa2d9f1680806b4814bba6474
seulhuiLee <75656859+leeseulhui@users.noreply.github.com>
2024-10-08 14:31:51 +0900

    [Bronze I] Title: 소인수분해, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -6,7 +6,14 @@ N = int(input().strip())
 if N == 1:
     exit()
 
-for i in range(2, N + 1):
+while N % 2 == 0:   # 소인수 홀수만 찾기 위해 2로 먼저 나눔
+    print(2)
+    N = N // 2
+
+for i in range(3, int(N**0.5) + 1, 2):
     while N % i == 0:
         print(i)
         N = N // i
+
+if N > 1:
+    print(N)
