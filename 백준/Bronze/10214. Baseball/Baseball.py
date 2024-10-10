9ecf0dbdcdfc41f9653cbb4a2456d4bbc24bbf5d
seulhuiLee <75656859+leeseulhui@users.noreply.github.com>
2024-10-10 14:37:11 +0900

    [Bronze III] Title: Baseball, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10214. Baseball/Baseball.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input().strip())
+
+for _ in range(T):
+    y_score = 0
+    k_score = 0
+
+    for _ in range(9):
+        y, k = map(int, input().split())
+        y_score += y
+        k_score += k
+
+    if y_score > k_score:
+        print("Yonsei")
+    elif y_score < k_score:
+        print("Korea")
+
+    else:
+        print("Draw")
+
+    
