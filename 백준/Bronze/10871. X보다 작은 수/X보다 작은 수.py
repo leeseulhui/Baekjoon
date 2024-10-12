54947b17ead2d12e60510124ca4c5e025df81c5f
seulhuiLee <75656859+leeseulhui@users.noreply.github.com>
2024-10-12 22:05:32 +0900

    [Bronze V] Title: X보다 작은 수, Time: 40 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -3,9 +3,9 @@ input = sys.stdin.readline
 
 N, X = map(int, input().split())
 
+
 A = list(map(int, input().split()))
 
-#수열 A에서 X보다 작은 값들 출력
 for num in A:
     if num < X:
-        print(num, end=' ')
+        print(num,end=' ')
