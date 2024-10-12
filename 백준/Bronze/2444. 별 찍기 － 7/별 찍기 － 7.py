22831a7f155f42dd369f128907fcdda9acd466bf
seulhuiLee <75656859+leeseulhui@users.noreply.github.com>
2024-10-12 10:50:12 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,16 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input().strip())
+n = int(input().strip())
 
-# 위쪽 삼각형
-for i in range(1, N + 1):
-    spaces = ' ' * (N - i) 
-    stars = '*' * (2 * i - 1) 
-    print(spaces + stars)
+# 상단
+for i in range(1, n+1):
+    print(' '*(n-i) + '*'*(2*i-1))
 
-# 아래쪽 삼각형
-for i in range(N + 1, 2 * N):
-    spaces = ' ' * (i - N) 
-    stars = '*' * (2 * (2 * N - i) - 1)
-    print(spaces + stars)
+# 하단
+for i in range(n-1, 0, -1):
+    print(' '* (n-i)+'*'*(2*i-1))
