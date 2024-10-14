6ac6d2057f2a3489bd14c20b9f8e1f7b02623bd0
seulhuiLee <75656859+leeseulhui@users.noreply.github.com>
2024-10-14 10:46:55 +0900

    [Bronze II] Title: 상수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,15 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-A, B = input().split()
+a, b = input().split()
 
-#숫자 거꾸로 뒤집어
-reversed_A = A[::-1]
-reversed_B = B[::-1]
+# 숫자 뒤집기
+a_reverse = a[::-1]
+b_reverse = b[::-1]
 
+a_reverse = int(a_reverse)
+b_reverse = int(b_reverse)
 
-reversed_A = int(reversed_A)
-reversed_B = int(reversed_B)
-
-answer = max(reversed_A, reversed_B)
-print(answer)
+ans = max(a_reverse, b_reverse)
+print(ans)
