0776df63e5542a88ef97b23bd663864d77d10e00
seulhuiLee <75656859+leeseulhui@users.noreply.github.com>
2024-10-16 14:27:43 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,16 +1,7 @@
 import sys
 input = sys.stdin.readline
 
-S = input().strip()
+s = input().strip()
 
-#리스트 초기화
-words = [-1] * 26
-
-#리스트 순회
-for index in range(len(S)):
-    char = S[index]
-    char_index = ord(char) - ord('a') 
-    if words[char_index] == -1: 
-        words[char_index] = index
-
-print(' '.join(map(str, words)))
+for char in range(ord('a'), ord('z') + 1):
+    print(s.find(chr(char)), end=' ')
