660b563f197a86850d74817c4dd558c36563dfd8
seulhuiLee <75656859+leeseulhui@users.noreply.github.com>
2024-10-17 10:45:12 +0900

    [Silver V] Title: 거스름돈, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+count = 0
+
+while n >= 0:
+    if n % 5 == 0:
+        count += n // 5
+        print(count)
+        break
+    n -= 2
+    count += 1
+else:
+    print(-1)
