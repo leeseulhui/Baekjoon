67248ea5688f83e9212f810e048c8cc8c6e07db5
seulhuiLee <75656859+leeseulhui@users.noreply.github.com>
2024-11-08 13:38:23 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 656 ms, Memory: 137220 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,14 +1,17 @@
 import sys
+input = sys.stdin.readline
 from collections import Counter
 
 n = int(input().strip())
-
-cards = list(map(int, input().strip().split()))
-card_count = Counter(cards) 
+cnt = 0
+cards = list(map(int, input().split()))
+cards_count = Counter(cards)
 
 m = int(input().strip())
+sang_cards = list(map(int, input().split()))
+result = [cards_count[sang_card] for sang_card in sang_cards]
 
-targets = list(map(int, input().strip().split()))
-result = [card_count[target] for target in targets]
+print(*result)
 
-print(*result)  # 공백으로 구분해서 출력
+
+
