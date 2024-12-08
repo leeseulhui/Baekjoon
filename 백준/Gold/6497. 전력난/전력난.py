7fda629f110a80a0c5a07c195a0bfe075a0f2116
seulhuiLee <75656859+leeseulhui@users.noreply.github.com>
2024-12-08 13:41:40 +0900

    [Gold IV] Title: 전력난, Time: 1220 ms, Memory: 61160 KB -BaekjoonHub

---
+++ 백준/Gold/6497. 전력난/전력난.py
@@ -0,0 +1,38 @@
+import sys
+
+input = sys.stdin.readline
+
+#유니온 파인드 알고리즘을 통해 부모 노드를 통일
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(x, y):
+    x, y = find(x), find(y)
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+
+
+while True:
+    m, n = map(int, input().split())
+    if m == 0 and n == 0:
+        break
+    parent = [i for i in range(m)]
+    cost = 0
+    edges = []
+    for _ in range(n):
+        u, v, w = map(int, input().split())
+        edges.append((u, v, w))
+    edges.sort(key=lambda x: x[2]) 
+
+    for edge in edges:
+        u, v, w = edge
+        if find(u) != find(v):
+            union(u, v)
+        else: 
+            cost += w
+    print(cost)
